8de9e62dc220cad304a62ca8b032c28a02181e7f
sahitmandapati <sahit.m@technovert.com>
2023-04-19 16:23:59 +0530

    Add project files.

---
+++ Library/Book.cs
@@ -0,0 +1,13 @@
+﻿namespace Library
+{
+    public class Book
+    {
+        
+        public Guid Id { get; set; } = Guid.NewGuid();
+        public string Title { get; set; } = null!;
+        public string Author { get; set; } = null!;
+        public int YearPublished { get; set; }
+        
+
+    }
+}
---
+++ Library/Controllers/BooksController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Library.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BooksController : ControllerBase
+    {
+        private readonly string _csvFilePath;
+
+        public BooksController()
+        {
+            _csvFilePath = $"{Environment.CurrentDirectory}/Properties/library.csv";
+        }
+
+        [HttpGet]
+        public IEnumerable<Book> GetAllBooks()
+        {
+            List<Book> books = new List<Book>();
+            var reader = new StreamReader(_csvFilePath);
+
+
+                while (!reader.EndOfStream)
+                {
+                    var line = reader.ReadLine();
+                    if (line is not null)
+                    {
+                        var values = line.Split(',');
+                        books.Add(new Book
+                        {
+                            Title = values[0],
+                            Author = values[1],
+                            YearPublished = Convert.ToInt32(values[2])
+                        });
+                    }
+
+                }
+            
+
+            return books;
+        }
+    }
+}
